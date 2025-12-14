d20dfe0cfb06aef2b591b8f32b8cb2b1a237be0a
Blake <wojobeemer@Blakes-MacBook-Pro-2.local>
2025-12-14 17:07:41 -0500

    Added calorie tracker progress and styles

--- frontend/nutrition-front-end/src/Calories.jsx
+++ frontend/nutrition-front-end/src/Calories.jsx
@@ -1,14 +1,33 @@
 import './assets/Calories.css'
 
-function Calories(){
-    return(
-        <div className="calories">
-            <h1 className="calorie-title">Total Calories</h1>
-            <svg width="100" height="50">
-                <circle r="45" cx="50" cy="50" fill="red"/> 
-            </svg>
-        </div>
-    );
+function Calories() {
+  const r = 190;
+  const cx = 200;
+  const cy = 210;
+
+  const x1 = cx - r;
+  const x2 = cx + r;
+  const y  = cy;
+
+  return (
+    <div className="calories">
+      <h1 className="calorie-title">Total Calories</h1>
+
+      <svg width="500" height="325" viewBox="0 0 400 300">
+        <path
+          d={`M ${x1} ${y} A ${r} ${r} 0 0 1 ${x2} ${y}`}
+          fill="none"
+          stroke="green"
+          strokeWidth="30"
+          strokeLinecap="round"
+        />
+      </svg>
+      <div className="calorie-number-ratio">
+        <span className="calorie-number">0</span>
+        <span className="calorie-ratio">0 / 3000</span>
+      </div>
+    </div>
+  );
 }
 
 export default Calories 
