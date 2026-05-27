7fc49ffcf93cbfa4db9385381f1258654356745b
Blake <wojobeemer@Blakes-MacBook-Pro-2.local>
2026-05-27 18:02:00 -0400

    fix: allow pages to be scrollable

--- backend/nutritionbackend/nutritionbackend/settings.py
+++ backend/nutritionbackend/nutritionbackend/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6j2^gedfp&@$nd7#y918vk4ps9j!k=suttje$vnam#aoys)wl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
